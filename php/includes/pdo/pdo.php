54b914bf30f6a4d703b75722de1c1cd505900e2d
YaMa <yama_rd@hotmail.com>
2021-04-12 10:31:38 +0200

    Update pdo.php

--- php/includes/pdo/pdo.php
+++ php/includes/pdo/pdo.php
@@ -2,7 +2,7 @@
     //C'est ici qu'il faut préciser le moyen d'accès à la base de données
     try {
         //PDO (hôte mysql, nom de la base de données, jeu de caractères, utilisateur, mot de passe, gestion des erreurs)
-        $database = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $database = new PDO('mysql:host=localhost;dbname=maestro;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch (Exception $e) {
         die('Error : ' . $e->getMessage());
